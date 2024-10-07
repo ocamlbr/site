0cdc245a42627348c4db2ef0057c469a5ef0c01d
Camilo Cunha de Azevedo <camilotk@gmail.com>
2024-10-07 18:22:33 -0300

    actions and redesign

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,35 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: "./_build/default/src/output/src/App.js",
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
+    publicPath: '/'
   },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public', to: '' }
+      ],
+    }),
+  ],
   devServer: {
-    headers: {
-      'Content-Type': 'text/html; charset=UTF-8',  // Define a codificação para UTF-8
-    },
     static: {
       directory: path.join(__dirname, 'public'),
+      publicPath: '/',
     },
     compress: true,
     port: 3000,
+    historyApiFallback: true,
   },
 };
-
